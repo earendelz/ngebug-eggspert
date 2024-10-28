bb44dd2f64b449c3f7df9503f8af48a1a10e371a
Farrel Fitra Mulyadi <farrelfimu@gmail.com>
2024-10-28 10:53:52 +0700

    Menambahkan API Kandang

---
+++ app/Http/Controllers/ProductAPIController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProductAPIController extends Controller
+{
+    public function index()
+    {
+        $userId = Auth::id();
+        $products = Product::where('id_peternak', $userId)->get();
+        return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        $userId = Auth::id();
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'capacity' => 'required|integer',
+            'chicken_count' => 'required|integer',
+            'chicken_breed' => 'required|string|max:255',
+        ]);
+
+        $product = Product::create([
+            'name' => $request->name,
+            'capacity' => $request->capacity,
+            'chicken_count' => $request->chicken_count,
+            'chicken_breed' => $request->chicken_breed,
+            'id_peternak' => $userId,
+        ]);
+
+        return response()->json($product, 201);
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return response()->json($product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'capacity' => 'required|integer',
+            'chicken_count' => 'required|integer',
+            'chicken_breed' => 'required|string|max:255',
+        ]);
+
+        $product->update($validated);
+
+        return response()->json($product);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GudangAPIController;
 use App\Http\Controllers\UserAPIController;
 use App\Http\Controllers\LaporanAyamAPIController;
+use App\Http\Controllers\ProductAPIController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +16,4 @@ Route::apiResource("user", UserAPIController::class);
 Route::get('/laporan-ayam/user_id/{id}', [LaporanAyamAPIController::class, 'getByUserId']);
 Route::get('/laporan-ayam/user/{username}', action: [LaporanAyamAPIController::class, 'getByUsername']); //buat manggil API berdasar username
 Route::post("/login", [UserAPIController::class, 'signIn']);
+Route::apiResource("products", ProductAPIController::class);
